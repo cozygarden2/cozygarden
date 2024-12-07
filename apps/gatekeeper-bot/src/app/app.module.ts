100c55fced04503905ec8bb3b8b6114229eb5b65
9hyd5xyxxb <9hyd5xyxxb@mail.ru>
2024-12-08 00:58:24 +0500

    dummy bot

--- apps/gatekeeper-bot/src/app/app.module.ts
+++ apps/gatekeeper-bot/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { AppUpdate } from './update.telegraf';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TelegrafModule.forRoot({
+      token: 'TELEGRAM_BOT_TOKEN',
+    })
+  ],
+  controllers: [AppController, AppUpdate],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/gatekeeper-bot/src/app/update.telegraf.ts
@@ -0,0 +1,33 @@
+import {
+  Update,
+  Ctx,
+  Start,
+  Help,
+  On,
+  Hears,
+} from 'nestjs-telegraf';
+
+type TelegrafContext = any;
+
+@Update()
+export class AppUpdate {
+  @Start()
+  async start(@Ctx() ctx: TelegrafContext) {
+    await ctx.reply('Welcome');
+  }
+
+  @Help()
+  async help(@Ctx() ctx: TelegrafContext) {
+    await ctx.reply('Send me a sticker');
+  }
+
+  @On('sticker')
+  async on(@Ctx() ctx: TelegrafContext) {
+    await ctx.reply('👍');
+  }
+
+  @Hears('hi')
+  async hears(@Ctx() ctx: TelegrafContext) {
+    await ctx.reply('Hey there');
+  }
+}
